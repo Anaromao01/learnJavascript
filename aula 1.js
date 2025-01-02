6e8073b7a8fa764597e341289e3d06fa7bc34a21
Anaromao01 <142629670+Anaromao01@users.noreply.github.com>
2025-01-02 11:38:27 -0300

    JavaScript

---
+++ aula 1.js
@@ -0,0 +1,26 @@
+/*
+
+  VARIÁVEIS
+   - let / const/ var( var foi descontinuado)
+   const -> Toda vez que eu crio uma variável com const, eu não posso alterar depos de atribuído
+   let -> Posso alterar o valor quantas vezes eu quiser
+
+IF(SE) / ELSE(Se não)
+   - Ajuda a ensinar nosso código a tomar decisões. condicional básica
+if(condição, se for verdadeira){
+faz isso
+} else {
+ se o if for falso, faça isso!
+}
+   
+
+FUNÇÕES 
+   - São um trecho de código, que só é excutado, quando é chamado
+
+LAÇOS
+   - For: é como uma roda gigante, gira várias vezes e cada volta é um ciclo.
+   - While: é uma repetição baseada em um condição, ou seja, enquanto for verdadeira continua rodando, se mudar para falsa, ela para.
+   - ForEach
+
+*/
+
